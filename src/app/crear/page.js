0e51a4b26f9c45d83ebc2ea3f34ba76fdb0aefeb
Rocasari <rsalazarr20_1@unc.edu.pe>
2024-11-23 11:33:54 -0500

    Base del proyecto

---
+++ src/app/crear/page.js
@@ -0,0 +1,11 @@
+import Card from "@/components/card";
+
+
+export default function Crear() {
+
+    let nombres = ["Rocasari", "Pinger"];
+    return (
+
+        <Card title="Carta de Prueba" />
+    );
+}
