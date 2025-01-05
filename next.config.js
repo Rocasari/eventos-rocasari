a24b886e52c8cf382129611540ec0371daf91cba
Rocasari <rsalazarr20_1@unc.edu.pe>
2025-01-05 17:20:25 -0500

    Corrigiendo errores

--- next.config.js
+++ next.config.js
@@ -3,8 +3,7 @@ module.exports = {
       return [
         {
           source: "/api/:path*", // Redirige todas las solicitudes que comiencen con /api
-          //destination: "http://192.168.10.13:8080/api/:path*", // URL del backend
-          destination: "http://192.168.10.2:8080/api/:path*", // URL del backend (Local)
+          destination: "http://192.168.10.13:8080/api/:path*", // URL del backend
         },
       ];
     },
