697ea39f7e87192e3e49fac44c5be03ec4fbcaae
Rocasari <rsalazarr20_1@unc.edu.pe>
2025-01-07 12:31:39 -0500

    Actualizar la configuración del ecosistema para mejorar la legibilidad y la estructura del archivo

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,16 @@
 module.exports = {
-    apps: [{
+  apps: [
+    {
       name: "eventos-rocasari",
       script: "npm",
       args: "start",
       log_date_format: "YYYY-MM-DD HH:mm:ss",
-      error_file: "~/.pm2/logs/eventos-rocasari-error.log",
-      out_file: "~/.pm2/logs/eventos-rocasari-out.log",
-      merge_logs: true,
+      error_file: "~/.pm2/logs/eventos-rocasari-error.log", // Logs de error.
+      out_file: "~/.pm2/logs/eventos-rocasari-out.log",     // Logs estándar.
+      merge_logs: true, // Combina logs si se usa clustering.
       env: {
-        NODE_ENV: "production"
-      }
-    }]
-  }
+        NODE_ENV: "production",
+      },
+    },
+  ],
+};
