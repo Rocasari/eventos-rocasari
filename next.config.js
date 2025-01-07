51b4491ca79ca357252739e2ffd28e42b53c3e46
Rocasari <rsalazarr20_1@unc.edu.pe>
2025-01-07 13:36:06 -0500

    Actualizar la URL de destino en la configuración de redirección de API

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
     return [
       {
         source: "/api/:path*", // Redirige todas las solicitudes que comiencen con /api
-        destination: "http://192.168.10.13:8080/api/:path*", // URL del backend
+        destination: "http://192.168.10.13:8080/api/:path*", // URL del backends
       },
     ];
   },
