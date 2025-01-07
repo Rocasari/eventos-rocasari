e7f2e98ab9ad4c55da8379ba95edd2d908530f17
Rocasari <rsalazarr20_1@unc.edu.pe>
2025-01-07 13:02:55 -0500

    Actualizar las rutas de los archivos de log en la configuración del ecosistema para mejorar la gestión de registros.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,8 +4,8 @@ module.exports = {
     script: "npm",
     args: "start",
     log_date_format: "YYYY-MM-DD HH:mm:ss",
-    error_file: "./logs/error.log",
-    out_file: "./logs/access.log",
+    error_file: "~/.pm2/logs/eventos-rocasari-error.log", // Logs de error.
+      out_file: "~/.pm2/logs/eventos-rocasari-out.log",
     merge_logs: true,
     env: {
       NODE_ENV: "production",
